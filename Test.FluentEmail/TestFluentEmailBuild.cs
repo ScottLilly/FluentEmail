0a08eda3a7d14f4476e78feae061b1c7faaf0dd3
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-07-13 20:41:57 -0500

    Added AddAttachment methods that accept streams, instead of only filenames

--- Test.FluentEmail/TestFluentEmailBuild.cs
+++ Test.FluentEmail/TestFluentEmailBuild.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Mime;
 using System.Net.Mail;
 using System.Text;
 using FluentEmail;
@@ -12,7 +13,7 @@ namespace Test.FluentEmail
         [Fact]
         public void Test_Instantiate_HtmlMailMessage()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateHtmlMailMessage()
                     .From("from@test.com")
@@ -24,19 +25,25 @@ namespace Test.FluentEmail
                     .AddAttachment(Path.Combine(".", "TestFiles", "filename1.txt"))
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
         }
 
         [Fact]
         public void Test_Instantiate_StringEmailAddresses()
         {
-            var test = 
+            var mailMessage = 
                 FluentMailMessage
                     .CreateMailMessage()
                     .From("from@test.com")
                     .To("qwe@test.com")
                     .To("zxc@test.com")
+                    .To(new List<string>
+                    {
+                        "mnb@test.com", 
+                        "qwe@test.com", // Duplicate, so should not be added
+                        "rty@test.com"
+                    })
                     .Subject("test")
                     .Body("This is the email body")
                     .AddAttachment(Path.Combine(".", "TestFiles", "filename1.txt"))
@@ -44,16 +51,17 @@ namespace Test.FluentEmail
                     .AddAttachment(Path.Combine(".", "TestFiles", "filename3.txt"))
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
-            Assert.NotNull(test.From);
-            Assert.Equal("from@test.com", test.From.Address);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.NotNull(mailMessage.From);
+            Assert.Equal("from@test.com", mailMessage.From.Address);
+            Assert.Equal(4, mailMessage.To.Count);
         }
 
         [Fact]
         public void Test_Instantiate_StringStringEmailAddresses()
         {
-            var test = 
+            var mailMessage = 
                 FluentMailMessage
                     .CreateMailMessage()
                     .From("from@test.com", "John From")
@@ -63,17 +71,17 @@ namespace Test.FluentEmail
                     .Body("This is the email body")
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
-            Assert.NotNull(test.From);
-            Assert.Equal("from@test.com", test.From.Address);
-            Assert.Equal("John From", test.From.DisplayName);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.NotNull(mailMessage.From);
+            Assert.Equal("from@test.com", mailMessage.From.Address);
+            Assert.Equal("John From", mailMessage.From.DisplayName);
         }
 
         [Fact]
         public void Test_Instantiate_StringStringEncodingEmailAddresses()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage()
                     .From("from@test.com", "John From", Encoding.UTF8)
@@ -83,35 +91,42 @@ namespace Test.FluentEmail
                     .Body("This is the email body")
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
-            Assert.NotNull(test.From);
-            Assert.Equal("from@test.com", test.From.Address);
-            Assert.Equal("John From", test.From.DisplayName);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.NotNull(mailMessage.From);
+            Assert.Equal("from@test.com", mailMessage.From.Address);
+            Assert.Equal("John From", mailMessage.From.DisplayName);
             // TODO: Pass in UTF-16 name and encoding, and verify
         }
 
         [Fact]
         public void Test_Instantiate_MailAddressEmailAddresses()
         {
-            var test = 
+            var mailMessage = 
                 FluentMailMessage
                     .CreateMailMessage()
                     .From(new MailAddress("from@test.com", "John From"))
                     .To(new MailAddress("qwe@test.com", "Sue Qwe"))
                     .To(new MailAddress("zxc@test.com"))
+                    .To(new List<MailAddress>
+                    {
+                        new MailAddress("mnb@test.com"),
+                        new MailAddress("qwe@test.com"), // Duplicate, so should not be added
+                        new MailAddress("rty@test.com")
+                    })
                     .Subject("test")
                     .Body("This is the email body")
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.Equal(4, mailMessage.To.Count);
         }
 
         [Fact]
         public void Test_Instantiate_StringEmailAddresses_WithDupes()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage()
                     .From("from@test.com")
@@ -128,18 +143,18 @@ namespace Test.FluentEmail
                     .AddAttachment(Path.Combine(".", "TestFiles", "filename3.txt"))
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
-            Assert.NotNull(test.From);
-            Assert.Equal("from@test.com", test.From.Address);
-            Assert.Equal(2, test.To.Count);
-            Assert.Equal(2, test.CC.Count);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.NotNull(mailMessage.From);
+            Assert.Equal("from@test.com", mailMessage.From.Address);
+            Assert.Equal(2, mailMessage.To.Count);
+            Assert.Equal(2, mailMessage.CC.Count);
         }
 
         [Fact]
         public void Test_Instantiate_StringStringEmailAddresses_WithDupes()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage()
                     .From("from@test.com", "John From")
@@ -156,22 +171,22 @@ namespace Test.FluentEmail
                     .Body("This is the email body")
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
-            Assert.NotNull(test.From);
-            Assert.Equal("from@test.com", test.From.Address);
-            Assert.Equal("John From", test.From.DisplayName);
-            Assert.Equal(2, test.To.Count);
-            Assert.Equal(2, test.CC.Count);
-            Assert.Equal(2, test.Bcc.Count);
-            Assert.Equal("test", test.Subject);
-            Assert.Equal("This is the email body", test.Body);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.NotNull(mailMessage.From);
+            Assert.Equal("from@test.com", mailMessage.From.Address);
+            Assert.Equal("John From", mailMessage.From.DisplayName);
+            Assert.Equal(2, mailMessage.To.Count);
+            Assert.Equal(2, mailMessage.CC.Count);
+            Assert.Equal(2, mailMessage.Bcc.Count);
+            Assert.Equal("test", mailMessage.Subject);
+            Assert.Equal("This is the email body", mailMessage.Body);
         }
 
         [Fact]
         public void Test_Instantiate_StringStringEncodingEmailAddresses_WithDupes()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage()
                     .From("from@test.com", "John From", Encoding.UTF8)
@@ -185,30 +200,31 @@ namespace Test.FluentEmail
                     .AddAttachment(Path.Combine(".", "TestFiles", "test2.txt"))
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
-            Assert.NotNull(test.From);
-            Assert.Equal("from@test.com", test.From.Address);
-            Assert.Equal("John From", test.From.DisplayName);
-            Assert.Equal(2, test.To.Count);
-            Assert.Equal("test", test.Subject);
-            Assert.Equal(Encoding.UTF8, test.SubjectEncoding);
-            Assert.Equal("This is the email body", test.Body);
-            Assert.Equal(Encoding.UTF8, test.BodyEncoding);
-            Assert.Equal(2, test.Attachments.Count);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.NotNull(mailMessage.From);
+            Assert.Equal("from@test.com", mailMessage.From.Address);
+            Assert.Equal("John From", mailMessage.From.DisplayName);
+            Assert.Equal(2, mailMessage.To.Count);
+            Assert.Equal("test", mailMessage.Subject);
+            Assert.Equal(Encoding.UTF8, mailMessage.SubjectEncoding);
+            Assert.Equal("This is the email body", mailMessage.Body);
+            Assert.Equal(Encoding.UTF8, mailMessage.BodyEncoding);
+            Assert.Equal(2, mailMessage.Attachments.Count);
             // TODO: Pass in UTF-16 name and encoding, and verify
         }
 
         [Fact]
         public void Test_Instantiate_MailAddressEmailAddresses_WithDupes()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage()
                     .From(new MailAddress("from@test.com", "John From"))
                     .To(new MailAddress("qwe@test.com", "Sue Qwe"))
                     .To(new MailAddress("qwe@test.com", "Sue Qwe"))
                     .To(new MailAddress("zxc@test.com", "asd name"))
+                    .To("ghj@test.com", "George H Jones", Encoding.UTF8)
                     .CC(new MailAddress("qwe123@test.com", "Sue123 Qwe"))
                     .CC(new MailAddress("qwe123@test.com", "Sue123 Qwe"))
                     .CC(new MailAddress("zxc123@test.com", "asd123 name"))
@@ -222,16 +238,62 @@ namespace Test.FluentEmail
                     })
                     .Build();
 
-            Assert.NotNull(test);
-            Assert.IsType<MailMessage>(test);
-            Assert.Equal(2, test.To.Count);
-            Assert.Equal(2, test.Attachments.Count);
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.Equal(3, mailMessage.To.Count);
+            Assert.Equal(2, mailMessage.Attachments.Count);
+        }
+
+        [Fact]
+        public void Test_AddAttachment_StringString()
+        {
+            var mailMessage =
+                FluentMailMessage
+                    .CreateHtmlMailMessage()
+                    .From("from@test.com")
+                    .To("qwe@test.com")
+                    .CC("lkj@test.com")
+                    .Subject("Hello")
+                    .Body("This is the email body")
+                    .AddAttachment(Path.Combine(".", "TestFiles", "filename1.txt"),
+                        "text/html")
+                    .AddAttachment(Path.Combine(".", "TestFiles", "filename1.txt"),
+                        "text/html")
+                    .AddAttachment(Path.Combine(".", "TestFiles", "filename2.txt"),
+                        "text/html")
+                    .Build();
+
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.Equal(2, mailMessage.Attachments.Count);
+        }
+
+        [Fact]
+        public void Test_AddAttachment_StringContentType()
+        {
+            var mailMessage =
+                FluentMailMessage
+                    .CreateHtmlMailMessage()
+                    .From("from@test.com")
+                    .To("qwe@test.com")
+                    .CC("lkj@test.com")
+                    .Subject("Hello")
+                    .Body("This is the email body")
+                    .AddAttachment(Path.Combine(".", "TestFiles", "filename1.txt"), 
+                        new ContentType(MediaTypeNames.Text.Plain))
+                    .AddAttachment(Path.Combine(".", "TestFiles", "filename1.txt"),
+                        new ContentType(MediaTypeNames.Text.Plain))
+                    .Build();
+
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.Single(mailMessage.Attachments);
         }
 
         [Fact]
         public void Test_Priority()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage()
                     .From(new MailAddress("from@test.com", "John From"))
@@ -241,13 +303,88 @@ namespace Test.FluentEmail
                     .Body("This is the email body")
                     .Build();
 
-            Assert.Equal(MailPriority.Normal, test.Priority);
+            Assert.Equal(MailPriority.Normal, mailMessage.Priority);
+        }
+
+        [Fact]
+        public void Test_AddAttachment_StreamName()
+        {
+            using FileStream fs1 = 
+                File.OpenRead(Path.Combine(".", "TestFiles", "filename1.txt"));
+            using FileStream fs2 =
+                File.OpenRead(Path.Combine(".", "TestFiles", "filename2.txt"));
+
+            var mailMessage =
+                FluentMailMessage
+                    .CreateHtmlMailMessage()
+                    .From("from@test.com")
+                    .To("qwe@test.com")
+                    .CC("lkj@test.com")
+                    .Subject("Hello")
+                    .Body("This is the email body")
+                    .AddAttachment(fs1, "filename1.txt")
+                    .AddAttachment(fs2, "filename2.txt")
+                    .Build();
+
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.Equal(2, mailMessage.Attachments.Count);
+        }
+
+        [Fact]
+        public void Test_AddAttachment_StreamNameMimeType()
+        {
+            using FileStream fs1 =
+                File.OpenRead(Path.Combine(".", "TestFiles", "filename1.txt"));
+            using FileStream fs2 =
+                File.OpenRead(Path.Combine(".", "TestFiles", "filename2.txt"));
+
+            var mailMessage =
+                FluentMailMessage
+                    .CreateHtmlMailMessage()
+                    .From("from@test.com")
+                    .To("qwe@test.com")
+                    .CC("lkj@test.com")
+                    .Subject("Hello")
+                    .Body("This is the email body")
+                    .AddAttachment(fs1, "filename1.txt", "text/html")
+                    .AddAttachment(fs2, "filename2.txt", "text/html")
+                    .Build();
+
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.Equal(2, mailMessage.Attachments.Count);
+        }
+
+        [Fact]
+        public void Test_AddAttachment_StreamContentType()
+        {
+            using FileStream fs1 =
+                File.OpenRead(Path.Combine(".", "TestFiles", "filename1.txt"));
+            using FileStream fs2 =
+                File.OpenRead(Path.Combine(".", "TestFiles", "filename2.txt"));
+
+            var mailMessage =
+                FluentMailMessage
+                    .CreateHtmlMailMessage()
+                    .From("from@test.com")
+                    .To("qwe@test.com")
+                    .CC("lkj@test.com")
+                    .Subject("Hello")
+                    .Body("This is the email body")
+                    .AddAttachment(fs1, new ContentType(MediaTypeNames.Text.Plain))
+                    .AddAttachment(fs2, new ContentType(MediaTypeNames.Text.Plain))
+                    .Build();
+
+            Assert.NotNull(mailMessage);
+            Assert.IsType<MailMessage>(mailMessage);
+            Assert.Equal(2, mailMessage.Attachments.Count);
         }
 
         [Fact]
         public void Test_PriorityHigh()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage(MailPriority.High)
                     .From(new MailAddress("from@test.com", "John From"))
@@ -257,13 +394,13 @@ namespace Test.FluentEmail
                     .Body("This is the email body")
                     .Build();
 
-            Assert.Equal(MailPriority.High, test.Priority);
+            Assert.Equal(MailPriority.High, mailMessage.Priority);
         }
 
         [Fact]
         public void Test_PriorityLow()
         {
-            var test =
+            var mailMessage =
                 FluentMailMessage
                     .CreateMailMessage(MailPriority.Low)
                     .From(new MailAddress("from@test.com", "John From"))
@@ -273,7 +410,7 @@ namespace Test.FluentEmail
                     .Body("This is the email body")
                     .Build();
 
-            Assert.Equal(MailPriority.Low, test.Priority);
+            Assert.Equal(MailPriority.Low, mailMessage.Priority);
         }
     }
 }
